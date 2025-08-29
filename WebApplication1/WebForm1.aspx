2a2d4198e37c4816d52b8682d4b8cae0c615c693
SUBHRANSHU SAHU <SUBHRANSHU SAHU@DESKTOP-NQE4C5H>
2025-08-29 17:41:42 +0530

    Add project files.

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div style="border-block">
+
+
+                <table class="auto-style1">
+                    <tr>
+                        <td>Rollno</td>
+                        <td>
+                            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Name</td>
+                        <td>
+                            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>class</td>
+                        <td>
+                            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Address</td>
+                        <td>
+                            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <asp:Button ID="Button1" runat="server" BackColor="Lime" ForeColor="Red" OnClick="Button1_Click" Text="Insert" />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <asp:Button ID="Button2" runat="server" BackColor="#3333FF" OnClick="Button2_Click" Text="update" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <asp:Button ID="Button3" runat="server" BackColor="#FF3300" ForeColor="White" OnClick="Button3_Click" Text="Delete" />
+                        </td>
+                    </tr>
+                </table>
+
+
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["userinfoConnectionString"].ConnectionString);
+
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            SqlCommand cmd = new SqlCommand("insert into userinfo (roll,name,class,addres) values (@roll,@name,@class,@addres)", con);
+
+            cmd.CommandType=CommandType.Text;
+            cmd.Parameters.AddWithValue("@roll", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@name", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@class", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@addres", TextBox4.Text);
+
+           if( cmd.ExecuteNonQuery() == 1)
+            {
+                Response.Write("<script>alert('Data Inserted Successfully')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Data Not Inserted')</script>");
+            }
+            con.Close();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["userinfoConnectionString"].ConnectionString);
+
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            SqlCommand cmd = new SqlCommand("UPDATE userinfo SET name = @name, class = @class, addres = @addres WHERE roll = @roll", con);
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@roll", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@name", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@class", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@addres", TextBox4.Text);
+
+            if (cmd.ExecuteNonQuery() == 1)
+            {
+                Response.Write("<script>alert('Data updated Successfully')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert(' no record found with this roll no')</script>");
+            }
+            con.Close();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["userinfoConnectionString"].ConnectionString);
+
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            SqlCommand cmd = new SqlCommand("DELETE userinfo  WHERE roll = @roll", con);
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@roll", TextBox1.Text);
+           
+
+            if (cmd.ExecuteNonQuery() == 1)
+            {
+                Response.Write("<script>alert('Data Delete Successfully')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert(' no record found with this roll no')</script>");
+            }
+            con.Close();
+
+        }
+    }
+}
